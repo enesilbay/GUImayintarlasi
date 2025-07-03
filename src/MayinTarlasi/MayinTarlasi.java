7307b194f9a09ab2f9a423009cb525d8d83a840e
Enes Ilbay <enes@example.com>
2025-07-03 15:50:44 +0300

    rastgele mayınlar ekler.

--- src/MayinTarlasi/MayinTarlasi.java
+++ src/MayinTarlasi/MayinTarlasi.java
@@ -2,6 +2,7 @@ package MayinTarlasi;
 
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MayinTarlasi {
@@ -18,15 +19,48 @@ public class MayinTarlasi {
 				 Btn b = new Btn(row,col);
 				 frame.add(b);
 				 board[row][col]=b;
-				 
-				 
+				}
 			 }
-			 
-		 }
-		 
+		 generateMine();
+		 print();
 		 frame.setVisible(true);
-		 
+	}
+	
+	public void generateMine() {
+		int i=0;
+		while(i<10) {
+			int randRow=(int) (Math.random() * board.length);
+			int randCol=(int) (Math.random()*board[0].length);
+			
+			while(board[randRow][randCol].isMine()) {  
+				 randRow=(int) (Math.random() * board.length);
+				 randCol=(int) (Math.random()*board[0].length);
+			}
+			board [randRow][randCol].setMine(true); 
+			i++;
+				}}
+	
+	public void print() {
+		
+		for(int row=0;row<board.length;row++) {
+			 for(int col=0;col<board[0].length;col++) {
+				  
+				 if(board[row][col].isMine()) {
+					 board[row][col].setIcon(new ImageIcon("src/mine.png"));
+				 }
+				 
+				 
+			 
+			 }}
+		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }
